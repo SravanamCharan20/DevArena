636bd12c8732fc540b8152ad36fcf42ab3bfe3ce
charan <sravanamcharan20@gmail.com>
2026-02-23 23:53:09 +0530

    Enhance Dashboard component with Socket.IO connection and user role handling

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -1,16 +1,34 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useUser } from "../utils/UserContext";
+import socket from "../utils/socket";
 
 const Dashboard = () => {
   const { user } = useUser();
-  if (!user) return;
+
+  useEffect(() => {
+    if (!socket.connected) {
+      socket.connect();
+    }
+
+    const onConnect = () => {
+      console.log(socket.id);
+    };
+
+    socket.on("connect", onConnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+    };
+  }, [user]);
+
+  if (!user) return null;
 
   const isAdmin = user.role === "admin";
+
   return (
     <div>
       {isAdmin && <button>Create Room</button>}
-
       <button>Join Room</button>
     </div>
   );
